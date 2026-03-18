bc13c2c5a7cf6d886d25520f44eca11f133680c9
n1nj4t4nuk1 <jpt73@pm.me>
2026-03-18 12:29:57 +0000

    Deploying to gh-pages from @ n1nj4t4nuk1/n1nj4t4nuk1.github.io@43cec443c15e834f62bca263f8b24c7e27d64379 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -67,42 +67,7 @@ ninja.data = [{
             },},{id: "news-the-new-personal-website-is-now-online-featuring-my-publications-projects-and-academic-information-a-centralized-space-to-share-my-work-in-cybersecurity-and-cyber-intelligence",
           title: 'The new personal website is now online, featuring my publications, projects, and academic...',
           description: "",
-          section: "News",},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-collapse",
-          title: 'Collapse',
-          description: "Server-side file compression service",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/collapse/";
-            },},{id: "projects-maruja",
+          section: "News",},{id: "projects-maruja",
           title: 'Maruja',
           description: "Docker container packet sniffer",
           section: "Projects",handler: () => {
